5c87b0e3b470b3b1b2f87b2378e3751c5c59a324
Nick <nickstich@icloud.com>
2020-06-28 09:39:53 +0300

    First commit

---
+++ src/main/java/com/actions/emislabbackend/EmisLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.actions.emislabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmisLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmisLabBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actions/emislabbackend/EmisLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actions.emislabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmisLabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
