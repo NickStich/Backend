bcc7e8a99ce92074286660d454944070107d9b9f
bilan.claudiu@gmail.com <bilan.claudiu@gmail.com>
2020-07-04 19:57:46 +0300

    added SpringBoot basic template (CRUD)

--- src/main/java/com/actions/emislabbackend/model/OperationTeeth.java
+++ src/main/java/com/actions/emislabbackend/model/OperationTeeth.java
@@ -1,25 +1,40 @@
 package com.actions.emislabbackend.model;
 
-import com.actions.emislabbackend.model.employees.*;
+
+import com.actions.emislabbackend.model.employees.Emi;
+import com.actions.emislabbackend.model.employees.EmployeeStrategy;
+import com.actions.emislabbackend.model.employees.Paula;
+import com.actions.emislabbackend.model.employees.Timeea;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.text.DecimalFormat;
+import javax.persistence.*;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Dental_jobs")
 public class OperationTeeth {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @Column
     private String medic;
+    @Column
     private String patient;
+    @Column
     private WorkType worktype;
+    @Column
     private int teethNr;
+    @Column
     private double timeea;
+    @Column
     private double paula;
+    @Column
     private double emi;
+
 
     public OperationTeeth(int id, String medic, String patient, WorkType worktype, int teethNr) {
         this.id = id;
